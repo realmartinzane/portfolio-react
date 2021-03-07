cc68f63cf4f173a5148b1026f0e3957e18be7ef1
Martin Zane <realmartinzane@gmail.com>
2021-03-07 19:07:13 +0100

    set up react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
 import { FC } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Home, Projects } from 'pages'
 
-export const App: FC = () => <div>Hello!</div>
+export const App: FC = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/projects">
+        <Projects />
+      </Route>
+    </Switch>
+  </Router>
+)
---
+++ src/pages/index.tsx
@@ -0,0 +1,2 @@
+export { Home } from './home'
+export { Projects } from './projects'
